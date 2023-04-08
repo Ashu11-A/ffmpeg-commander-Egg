d8b8299664a9a14ebc7d4e06f1ca10676747e381
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-04-08 17:59:29 -0400

    Fix

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  devServer: {
+    // 'auto' | 'all' [string] here
+    allowedHosts: 'all',
+  },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/ffmpeg-commander/'
     : '/',
